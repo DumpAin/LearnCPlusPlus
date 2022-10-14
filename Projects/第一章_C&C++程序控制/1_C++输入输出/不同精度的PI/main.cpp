5f251824b9a59960630e1a446c2fd981193669e1
DumpA1n <87346356+DumpAin@users.noreply.github.com>
2022-10-15 00:08:17 +0800

    Update main.cpp

--- Projects/第一章_C&C++程序控制/1_C++输入输出/不同精度的PI/main.cpp
+++ Projects/第一章_C&C++程序控制/1_C++输入输出/不同精度的PI/main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include <math.h>
-// 包含流操作算子库
 #include <iomanip>
 using namespace std;
 
-// 定义常量PI，后面可以直接用PI代替后面的数值
 #define PI 3.14159265358979323846
 
 int main()
 {
-    int n;
-    cin >> n;
-    //n = pow(n,3);
-    cout << n;
-    
+    int n = 0, max = 0;
+    cin >> n;        
+	max = n + 4;
+	for (n; n <= max; n++)
+	    cout << setiosflags(ios::fixed) << setprecision(n) << PI << endl;
+    return 0;
 }
